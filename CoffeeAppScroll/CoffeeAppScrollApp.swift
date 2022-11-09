467337600dcb9fabb94af3308abd2058005024d8
ihValery <ihValery@gmail.com>
2022-11-09 22:23:19 +0300

    Initial Commit

---
+++ CoffeeAppScroll/CoffeeAppScrollApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeAppScrollApp.swift
+//  CoffeeAppScroll
+//
+//  Created by Валерий Игнатьев on 9.11.22.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeAppScrollApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

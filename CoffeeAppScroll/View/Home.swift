10388d34c96f03a09e92643d7b59485f1464fb9d
ihValery <ihValery@gmail.com>
2022-11-10 00:15:12 +0300

    Crash Xcode

--- CoffeeAppScroll/View/Home.swift
+++ CoffeeAppScroll/View/Home.swift
@@ -10,6 +10,14 @@ import SwiftUI
 //MARK: - Home
 
 struct Home: View {
+    
+    //MARK: Properties
+    
+    @State private var offsetY: CGFloat = .zero
+    @State private var currentIndex: CGFloat = .zero
+    
+    //MARK: Body
+    
     var body: some View {
         GeometryReader {
             let size = $0.size
@@ -25,7 +33,38 @@ struct Home: View {
             }
             .frame(width: size.width)
             .padding(.top, size.height - cardSize)
+            .offset(y: offsetY)
+            .offset(y: -currentIndex * cardSize)
         }
+        .coordinateSpace(name: "SCROLL")
+        .contentShape(Rectangle())
+        .gesture(
+            DragGesture()
+                .onChanged({ value in
+                    //Slowing down the gesture
+                    //Замедление жеста
+                    offsetY = value.translation.height * 0.4
+                })
+                .onEnded({ value in
+                    let translation = value.translation.height
+                    
+                    withAnimation(.easeInOut) {
+                        if translation > 0 {
+                            //250 Update it for your own useage
+                            //Обновите его для собственного использования
+                            if currentIndex > 0 && translation > 250 {
+                                currentIndex -= 1
+                            }
+                        } else {
+                            if currentIndex < CGFloat(coffees.count - 1) && -translation > 250 {
+                                currentIndex += 1
+                            }
+                        }
+                        
+                        offsetY = .zero
+                    }
+                })
+        )
         .preferredColorScheme(.light)
     }
 }
@@ -46,15 +85,32 @@ struct CoffeView: View {
     var size: CGSize
     
     //MARK: Body
-
+    
     var body: some View {
+        let cardSize = size.width
+        //Since i want to show max card on the display
+        let maxCarsdDisplaySize = size.width * 3
+        
         GeometryReader { proxy in
-            let size = proxy.size
+            let _size = proxy.size
+            //Scalling animation
+            //CurrentCardOffset
+            let offset = proxy.frame(in: .named("SCROLL")).minY - (size.height - cardSize)
+            let scale = offset <= 0 ? (offset / maxCarsdDisplaySize) : 0
+            let reduceScale = 1 + scale
+            let currentCardScale = offset / cardSize
             
             Image(coffee.imageName)
                 .resizable()
                 .aspectRatio(contentMode: .fit)
-                .frame(width: size.width, height: size.height)
+                .frame(width: _size.width, height: _size.height)
+                //To avoid warning
+                //Updating anchor based on the current card scale
+                .scaleEffect(reduceScale < 0 ? 0.001 : reduceScale,
+                             anchor: .init(x: 0.5, y: 1 - (currentCardScale / 2.4)))
+            
+            
+            Text("\(offset)")
         }
         .frame(height: size.width)
     }
